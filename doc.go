e931212e8443765d5b1f7ceacfaa1754cf130d2f
Christopher McMillon <Christopher.McMillon@halliburton.com>
2019-05-08 15:12:44 -0500

    Added dep files

--- doc.go
+++ doc.go
@@ -1,2 +1,7 @@
 // Package for wrapping the well-know types into a go module for download into project.
 package proto
+
+import (
+	_ "github.com/ProfessorMc/proto/protbuf"
+	_ "github.com/ProfessorMc/proto/protbuf/compiler"
+)
